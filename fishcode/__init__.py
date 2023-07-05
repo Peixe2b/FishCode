eccc221e460728a957fb4e15ed5a89dc669b5205
Fish <109036036+Peixe2b@users.noreply.github.com>
2023-07-05 13:14:20 -0300

    Update __init__.py

--- fishcode/__init__.py
+++ fishcode/__init__.py
@@ -1,14 +1,18 @@
-from fishcode.src.window import WINDOW
+from fishcode.src.window import WINDOW, GLFW
 from fishcode.src.game import Game
 
 def run(game: Game):
     WINDOW.init()
     game.init()
 
-    while WINDOW.running:
+    while not GLFW.window_should_close(WINDOW.window):
         WINDOW.update()
         game.update()
         game.draw()
 
-def exit_game(window):
-    window.running = False
+def exit_game():
+    GLFW.terminate()
+    WINDOW.running = False
+
+def glfw_api_version():
+    print(GLFW.api_version())
